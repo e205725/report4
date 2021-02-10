99373f21fbb4601e7a09424089847afd573899e3
Namiki Teruya <e205725@ie.u-ryukyu.ac.jp>
2021-02-10 18:25:20 +0900

    end commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e205725/Warrior.java
@@ -0,0 +1,16 @@
+package jp.ac.uryukyu.ie.e205725;
+
+public class Warrior extends Hero {
+    public Warrior(String name,int maximumHP,int attack){
+        super(name,maximumHP,attack);
+    }
+    public void attackWithWeponSkill(LivingThing opponent){
+        if(getHitPoint() <= 0){
+            return;
+        }
+        int damage = (int)(getAttack()*1.5);
+        System.out.printf("%sの攻撃！ウェポンスキルを発動！%sに%dのダメージを与えた!!\n",getName(),opponent.getName(),damage);
+        opponent.wounded(damage);
+    }
+}
+
